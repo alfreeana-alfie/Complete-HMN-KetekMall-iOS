d9936d48ef08114ff4eeb890cd111e658c7a846c
alfreeana-alfie <61505268+alfreeana-alfie@users.noreply.github.com>
2020-09-24 00:22:15 +0800

    update ipay & iPayPayment

---
+++ ketekmall4/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  ketekmall4
+//
+//  Created by Alfreeana Alfie on 24/09/2020.
+//  Copyright © 2020 Alfreeana Alfie. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
