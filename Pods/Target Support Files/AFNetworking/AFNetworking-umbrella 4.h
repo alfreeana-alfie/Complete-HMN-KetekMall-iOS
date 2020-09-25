9039ee0d800a899be937ca631ed2c499e5e8acfa
alfreeana-alfie <61505268+alfreeana-alfie@users.noreply.github.com>
2020-09-25 13:19:15 +0800

    updates

---
+++ Pods/Target Support Files/AFNetworking/AFNetworking-umbrella 4.h
@@ -0,0 +1,44 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AFNetworking.h"
+#import "AFHTTPSessionManager.h"
+#import "AFURLSessionManager.h"
+#import "AFCompatibilityMacros.h"
+#import "AFNetworkReachabilityManager.h"
+#import "AFSecurityPolicy.h"
+#import "AFURLRequestSerialization.h"
+#import "AFURLResponseSerialization.h"
+#import "AFAutoPurgingImageCache 17.h"
+#import "AFAutoPurgingImageCache.h"
+#import "AFImageDownloader 17.h"
+#import "AFImageDownloader.h"
+#import "AFNetworkActivityIndicatorManager 17.h"
+#import "AFNetworkActivityIndicatorManager.h"
+#import "UIActivityIndicatorView+AFNetworking 20.h"
+#import "UIActivityIndicatorView+AFNetworking.h"
+#import "UIButton+AFNetworking 32.h"
+#import "UIButton+AFNetworking.h"
+#import "UIImageView+AFNetworking 42.h"
+#import "UIImageView+AFNetworking.h"
+#import "UIKit+AFNetworking 46.h"
+#import "UIKit+AFNetworking.h"
+#import "UIProgressView+AFNetworking 46.h"
+#import "UIProgressView+AFNetworking.h"
+#import "UIRefreshControl+AFNetworking 46.h"
+#import "UIRefreshControl+AFNetworking.h"
+#import "WKWebView+AFNetworking 46.h"
+#import "WKWebView+AFNetworking.h"
+
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
+
